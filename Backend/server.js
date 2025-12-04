36ae787162d77bc7f1e7b037df769744d982fd9f
varunseth578 <ivarunseth@gmail.com>
2025-12-04 12:24:57 +0530

    cover Image

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const PORT=process.env.PORT || 3000;
 connectDB();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 app.use("/",require("./routes/user"));
 app.use("/recipe",require("./routes/recipe"));
 
--- Frontend/src/comp/Recipeitems.jsx
+++ Frontend/src/comp/Recipeitems.jsx
@@ -15,7 +15,7 @@ export default function Recipeitems() {
         {allRecipes?.map((item, index) => {
           return (
             <div key={index} className="card">
-              <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVjaXBlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" width="120px" height="100px" />
+              <img src={`http://localhost:5000/images/${item.coverImage}`} width="120px" height="100px" />
               <div className='card-body'>
                 <div className='title'>{item.title}</div>
                 <div className='icons'>
